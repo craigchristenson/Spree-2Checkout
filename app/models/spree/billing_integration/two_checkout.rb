f1d4ad0be2710f724cc06390f5449fb01219a45f
craig <craig@lappy.(none)>
2013-04-13 22:38:33 -0400

    direct checkout

--- app/models/spree/billing_integration/two_checkout.rb
+++ app/models/spree/billing_integration/two_checkout.rb
@@ -2,8 +2,9 @@ module Spree
   class BillingIntegration::TwoCheckout < BillingIntegration
 	preference :sid, :string
 	preference :secret_word, :string
+        preference :direct_checkout, :boolean, :default => true
 
-	attr_accessible :preferred_sid, :preferred_secret_word, :preferred_server, :preferred_test_mode
+	attr_accessible :preferred_sid, :preferred_secret_word, :preferred_direct_checkout, :preferred_server, :preferred_test_mode
 
 	def provider_class
 	  ActiveMerchant::Billing::Integrations::TwoCheckout
